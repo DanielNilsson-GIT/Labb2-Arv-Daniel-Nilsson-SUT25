d4f68e048020c672f6a99fc8f9c79bd9980953d1
DanielNilsson-GIT <daniel.gronberg92@gmail.com>
2025-10-24 16:21:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Labb2_Arv_Daniel_Nilsson_SUT25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2_Arv_Daniel_Nilsson_SUT25
+{
+    internal class Vehicle
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public int Miles { get; set; }
+    }
+}
