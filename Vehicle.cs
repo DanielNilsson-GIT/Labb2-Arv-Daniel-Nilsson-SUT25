d6717ee08c23f749f330fa697a16a6d871361dfe
DanielNilsson-GIT <daniel.gronberg92@gmail.com>
2025-10-24 16:32:36 +0200

    added 5 props and a constructor do parent class

--- Vehicle.cs
+++ Vehicle.cs
@@ -10,7 +10,22 @@ namespace Labb2_Arv_Daniel_Nilsson_SUT25
     {
         public string Brand { get; set; }
         public string Model { get; set; }
-
+        public int NoOfWheels { get; set; }
+        public string CountryOfOrigin { get; set; }
         public int Miles { get; set; }
+
+        public Vehicle(string brand, string model, int noofwheels, string countryoforigin, int miles)
+        {
+            Brand = brand;
+            Model = model;
+            NoOfWheels = noofwheels;
+            CountryOfOrigin = countryoforigin;
+            Miles = miles;
+
+        }
+        public void Drive()
+        {
+
+        }
     }
 }
